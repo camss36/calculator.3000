f5527df1a06c8845fbfe0cee1e9668a04bd7351e
camss36 <camillamaia36@gmail.com>
2022-07-11 16:40:46 -0300

    up

--- calculoAreaTriEquilatero.js
+++ calculoAreaTriEquilatero.js
@@ -1,9 +1,7 @@
 function areatriequilátero() {
     var ladoequi = document.getElementById("ate").value
 
-    var calequi = ((ladoequi ** ladoequi) ** 1.73) /4 
+    var calequi = ((ladoequi ** ladoequi) ** 1.73) /4;
 
-    var total2 = calequi.toFixed(2);
-
-    document.getElementById('texto2').innerHTML = total2
+    texto2.innerHTML= `${calequi} o que quiser que apareça na tela`
 }
---
+++ openclose.js
@@ -0,0 +1,26 @@
+function openimc() {
+    document.getElementById("containerIMC").style.display = "block";
+}
+
+function closeimc() {
+     if (document.getElementById("containertriequi").style.display = "block") {
+        document.getElementById("containertriequi").style.display = "none";
+     }
+
+     if (document.getElementById("containertrapezio").style.display = "block") {
+        document.getElementById("containertrapezio").style.display = "none";
+     }
+
+     if (document.getElementById("containerretangulo").style.display = "block") {
+        document.getElementById("containerretangulo").style.display = "none";
+     }
+
+     if (document.getElementById("containerquadrado").style.display = "block") {
+        document.getElementById("containerquadrado").style.display = "none";
+     }
+
+     if (document.getElementById("calculador").style.display = "block") {
+        document.getElementById("calculador").style.display = "none";
+     }
+
+}
